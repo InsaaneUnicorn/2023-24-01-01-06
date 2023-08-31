45ec1db0058125c75657086228c281cd7ec5e42a
InsaaneUnicorn <123391363+InsaaneUnicorn@users.noreply.github.com>
2023-08-31 14:43:40 +0000

    test commit
    test

--- main.c
+++ main.c
@@ -20,11 +20,6 @@
 #include <stdbool.h>
 
 
-char ANTI_GOOGLE[206][1000] = {
-"Google Chrome","Google Account","YouTube","Gmail","Google Play","Google Search","Google AdSense","Pixel Phone","Google Maps","Google Cloud","Google Ads","Google Photos","Google For Families","Google Fi Wireless","Google Nest","Google Pay","Google Store","Google Drive","Google Shopping","Google Accessibility","Blogger","Finance","Google Earth","Google Maps","Gmail","Picasa","Toolbar","Google Search","Google Calendar","Google Docs Editors","Books","Google Groups","YouTube","Trends","Android","Sites","Google Chrome","ChromeOS","ChromiumOS","Fuscia","GLinux","Google Gulp","Cardboard Plastic","Tango","Gerrit","Google Drive","Google Voice","Google Translate","Chromebook","Google TV","Google Play","My Maps","Chrome Web Store","Travel","Google Fiber","Nexus","Ads","Nik Collection","Chromecast","Snapseed","Google Keep","Starbucks WiFi","Google Fi Wireless","Waze","Wear OS by Google","Google Fit","YouTube For Families","Google Store","Chromecast built-in","Google Photos","Android Auto","Android TV","Google For Families","Google Camera","Gcam","Google Wifi","Waymo","Local Guides","YouTube Music","YouTube Studio App Help Center","Google Cardboard","Google Duo","Datally","Google Nest","YouTube TV","Google Clips","Pixel Phone","Opinion Rewards","Daydream","Google Assistant","Files by Google","CS First","Google Chat","Pixelbook","Messages","Reserve with Google","Phone app","Gboard","Contacts","Google One","Google Tasks","Google News","Google Accessibility","Science Journal","Google Shopping","Stadia","Google Pay","Google Food Ordering","Google Kids Space","User Security","Google Workspace Individual","Google Pixel Watch","Google Wallet","AR","Google Pixel Tablet","Google Ads Editor","Google Workspace Admin","Books","Search Console","Programmable Search Engine","Publisher Center","Ad Grants","Google Workspace","Google Ads","Google Cloud","Studio","Google AdSense","Authorized Buyers","Maps Content Partners","Google Merchant Center","Google Ad Manager","Analytics","Currents","Transit Partners","Global Market Finder","Google for Nonprofits","Search Ads 360","Chrome Enterprise and Education","Google AdMob","Google Vault","Google Business Profile","Hotel Center","Display & Video 360","Campaign Manager 360","Google Domains","Google Pay Merchant","Google Cloud Platform Console","Google Arts & Culture Platform","Android Enterprise","Cloud Search","Google Marketing Platform","Cardboard Manufacturer","Card Issuer","Google Meet","Jamboard","Google Pay for Online Business","Local Services","Work Insights","Comparison Shopping Services Center","Elastifile","CEWA","AppSheet","Ads Creative Studio","Orion Wifi","Play Console","GDG","Google Web Designer","Cast Developer","reCAPTCHA","Payments center","Payment Processor","Chromium","Sundar","Susan","Bard","Launcher3","Google Glass","Rick","Mountain View","Googleplex","Gbike","Lollipop","Oreo","Nougat","Marshmallow","Kitkat","Honeycomb","Jellybean","Cupcake","Pie","Ice Cream Sandwich","Gingerbread","Froyo","Eclair","Petit Four","Red Velvet Cake","Quince Tart","Snow Cone","Upside Down Cake","Vanilla Ice Cream","Charlie's Cafe","Shorline","Ampitheater","I/O","stack overflow"
-}; // add google colors & their hex values
-
-
 
 int main() {
 	showMainMenu();	
@@ -33,7 +28,7 @@ int main() {
     do{
       int remainingGuesses = 6;
       srand (time (NULL));
-      printf (" Welcome to HangWaites! (H.A.N.G.W.A.I.T.E.S. - Hilarious Antics of Nervous Guests Waiting And Imagining The Endless Scenarios)\nWhitespaces are underscores btw\n");
+      printf ("Welcome to HangWaites! (H.A.N.G.W.A.I.T.E.S. - Hilarious Antics of Nervous Guests Waiting And Imagining The Endless Scenarios)\nWhitespaces are underscores btw\n");
       Which_Hangman(1);
 				
       // Get word to guess
@@ -41,11 +36,13 @@ int main() {
       fp = fopen("wordbank.txt", "r+");
       
       
-      int lines = 10;
+      int lines = 0;
       char buf[8];
-      char contents[ = fgets(buf, sizeof buf, fp);
-      
-        
+      char *contents;
+      while (fgets(buf, sizeof buf, fp) != NULL) {
+        strcpy(contents, buf);
+      }
+      printf("%s", contents);
     /*
 char ** data = malloc(lines * sizeof(char *));
       
@@ -63,10 +60,9 @@ char ** data = malloc(lines * sizeof(char *));
         
       while(token) {
         lines++;
-        token = strtok(NULL, " ");
+        token = strtok(NULL, "\n");
       }
       printf("%d", lines);
-  
       fclose(fp);
   
      
@@ -115,9 +111,10 @@ char ** data = malloc(lines * sizeof(char *));
         }
 
         
-        if(isInList(guess, ANTI_GOOGLE)){
+        /*if(isInList(guess, ANTI_GOOGLE)){
           gameover = 1;
-        }
+        }*/
+        
         // Determine whether the player has won!
         gameover = 1;
         for(int m = 0; m < N; ++m) {
